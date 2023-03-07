407314359164c283c91d4a899c946c4655cf13bd
SakshiDhanawade25 <119151575+SakshiDhanawade25@users.noreply.github.com>
2023-03-07 11:03:13 +0530

    Update todolist.js

--- todolist.js
+++ todolist.js
@@ -1,71 +1,38 @@
-window.addEventListener('load', ()=>{
-    const form=document.querySelector("#asktask");
-    const input=document.querySelector("#input");
-    const list_el=document.querySelector("#tasks");
+const input = document.querySelector('input');
+const btn = document.querySelector('.addTask > button');
 
-    form.addEventListener('submit',(e) =>{
-        e.preventDefault();
+btn.addEventListener('click', addList);
+input.addEventListener('keyup', (e) => {})
 
-        const task=input.value;
-        
-        if(!task){ 
-            alert("Please Enter the Task.");
-            return;
-        }
-        
+function addList(e) {
+    const notCompleted = document.querySelector('.notCompleted');
+    const Completed = document.querySelector('.Completed');
 
-        const task_el =document.createElement("div");
-        task_el.classList.add("add_task");
+    const newLi = document.createElement('li');
+    const checkBtn = document.createElement('button');
+    const delBtn = document.createElement('button');
 
-        const task_content_el=document.createElement("div");
-        task_content_el.classList.add("content");
-      
-        
-        task_el.appendChild(task_content_el);
+    checkBtn.innerHTML = '<i class="fa fa-check"></i>';
+    delBtn.innerHTML = '<i class="fa fa-trash"></i>';
 
-        const task_input_el=document.createElement("input");
-        task_input_el.classList.add("text");
-        task_input_el.type="text";
-        task_input_el.value=task;
-        task_input_el.setAttribute("readonly","readonly");
 
-        task_content_el.appendChild(task_input_el);
+    if (input.value !== '') {
+        newLi.textContent = input.value;
+        input.value = '';
+        notCompleted.appendChild(newLi);
+        newLi.appendChild(checkBtn);
+        newLi.appendChild(delBtn);
+    }
 
-        const task_actions_el=document.createElement("div");
-        task_actions_el.classList.add("action");
-
-        const task_edit_el=document.createElement("button");
-        task_edit_el.classList.add("edit");
-        task_edit_el.innerHTML="Edit";
-
-        const task_delete_el=document.createElement("button");
-        task_delete_el.classList.add("delete");
-        task_delete_el.innerHTML="Delete";
-
-        task_actions_el.appendChild(task_edit_el);
-        task_actions_el.appendChild(task_delete_el);
-
-        task_el.appendChild(task_actions_el);
-
-        list_el.appendChild(task_el);
-        
-        input.value = "";//for clear
-        
-        //for edit
-        task_edit_el.addEventListener('click',()=>{
-            if(task_edit_el.innerText.toLowerCase() =="edit"){
-                task_input_el.removeAttribute("readonly");
-                task_input_el.focus();
-                task_edit_el.innerText="save";
-            }else{
-                task_input_el.setAttribute("readonly","readonly");
-                task_edit_el.innerHTML="Edit";
-            }
-        }); 
-
-        //for delete
-        task_delete_el.addEventListener('click',()=>{
-            list_el.removeChild(task_el);
-        });
+    checkBtn.addEventListener('click', function() {
+        const parent = this.parentNode;
+        parent.remove();
+        Completed.appendChild(parent);
+        checkBtn.style.display = 'none';
     });
-});
+
+    delBtn.addEventListener('click', function() {
+        const parent = this.parentNode;
+        parent.remove();
+    });
+}
